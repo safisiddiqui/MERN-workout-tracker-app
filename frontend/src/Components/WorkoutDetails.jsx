f8c3bef51fcab30a475228369cfb625bc6810311
Safi <safisiddiquim@gmail.com>
2022-07-31 01:44:05 -0500

    finished workout tracker app

---
+++ frontend/src/Components/WorkoutDetails.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleDelete = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (lb): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span onClick={handleDelete}>delete</span>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
